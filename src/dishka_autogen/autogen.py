2da6d20724672e2275aea34c4bcc533df209e56c
Danil Kovalev <dan.kovalev2013@gmail.com>
2026-04-11 10:11:01 +0300

    feat: adding event for session scope

--- src/dishka_autogen/autogen.py
+++ src/dishka_autogen/autogen.py
@@ -1,20 +1,14 @@
-__all__ = (
-    "CONTAINER_NAME",
-    "SESSION_CONTAINER_NAME",
-    "AG2Provider",
-    "DishkaMiddleware",
-    "inject",
-)
+__all__ = ("AG2Provider", "DishkaMiddleware", "inject")
 
 from autogen.beta.context import Context
-from autogen.beta.events import ToolCallEvent
+from autogen.beta.events import BaseEvent, ToolCallEvent
 from dishka import Provider, Scope, from_context
 
-from dishka_autogen._consts import CONTAINER_NAME, SESSION_CONTAINER_NAME
 from dishka_autogen._injectors import inject
 from dishka_autogen._middleware import DishkaMiddleware
 
 
 class AG2Provider(Provider):
+    initial_event = from_context(BaseEvent, scope=Scope.SESSION)
     context = from_context(Context, scope=Scope.SESSION)
     tool_call = from_context(ToolCallEvent, scope=Scope.REQUEST)
