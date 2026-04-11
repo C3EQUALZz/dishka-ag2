5ced8930afd90a5756289c210555a92b8b0b65f9
Danil Kovalev <dan.kovalev2013@gmail.com>
2026-04-11 10:10:02 +0300

    fix: fixing allowed objects form library

--- src/dishka_autogen/__init__.py
+++ src/dishka_autogen/__init__.py
@@ -1,18 +1,5 @@
-__all__ = (
-    "CONTAINER_NAME",
-    "SESSION_CONTAINER_NAME",
-    "AG2Provider",
-    "DishkaMiddleware",
-    "FromDishka",
-    "inject",
-)
+__all__ = ("AG2Provider", "DishkaMiddleware", "FromDishka", "inject")
 
 from dishka import FromDishka
 
-from .autogen import (
-    CONTAINER_NAME,
-    SESSION_CONTAINER_NAME,
-    AG2Provider,
-    DishkaMiddleware,
-    inject,
-)
+from .autogen import AG2Provider, DishkaMiddleware, inject
