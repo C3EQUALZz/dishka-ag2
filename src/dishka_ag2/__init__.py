24e18bcb0778cb7e7c31945e424489faea00fa10
Danil Kovalev <dan.kovalev2013@gmail.com>
2026-04-11 11:09:45 +0300

    chore: renaming package to dishka_ag2

--- src/dishka_ag2/__init__.py
+++ src/dishka_ag2/__init__.py
@@ -2,4 +2,4 @@ __all__ = ("AG2Provider", "DishkaMiddleware", "FromDishka", "inject")
 
 from dishka import FromDishka
 
-from .autogen import AG2Provider, DishkaMiddleware, inject
+from .ag2 import AG2Provider, DishkaMiddleware, inject
